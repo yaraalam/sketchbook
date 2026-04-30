ff5574053c7de64fed8a26e6e6b6f2c6fdb39f5e
yara <52312801+yaraalam@users.noreply.github.com>
2026-04-30 05:05:10 +0000

    made changes all aspects allowing for more sketches

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,13 @@
 let sketches = document.querySelectorAll(".sketch");
 
 sketches.forEach(sketch => {
+  sketch.addEventListener("click", () => {
 
-    sketch.addEventListener("click", () => {
+    sketch.style.left = Math.random() * 70 + "%";
+    sketch.style.top = Math.random() * 70 + "%";
 
-        sketch.style.left = Math.random() * 70 + "%";
-        sketch.style.top = Math.random() * 70 + "%";
+    sketch.style.transform =
+      `rotate(${Math.random() * 60 - 30}deg) scale(1.1)`;
 
-        sketch.style.transform =
-        `rotate(${Math.random() * 40 - 20}deg)`;
-
-    });
-
+  });
 });
